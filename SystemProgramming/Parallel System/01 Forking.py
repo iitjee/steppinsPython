38b7f41491bdbdb7a634b5f49bf5d86edb234701
iitjee <iit.jee.help.me@gmail.com>
2017-09-27 00:40:39 +0530

    Update 01 Forking.py

--- SystemProgramming/Parallel System/01 Forking.py
+++ SystemProgramming/Parallel System/01 Forking.py
@@ -45,3 +45,43 @@ processing module, provides an alternative for running processes portably )
 Note: Above script does work on Windows, however, if you use the Python shipped with the Cygwin system (or build one of your 
 own from source-code with Cygwin’s libraries). Cygwin is a free, open source system that provides full Unix-like 
 functionality for Windows 
+
+A subtle point: the child process function is also careful to exit explicitly with an os._exit call. We’ll discuss this call 
+ in more detail later in this chapter, but if it’s not made, the child process would live on after the child function r
+ eturns (remember, it’s just a copy of the original process). The net effect is that the child would go back to the loop in 
+ parent and start forking children of its own (i.e., the parent would have grandchildren). If you delete the exit call and 
+ rerun, you’ll likely have to type more than one q to stop, because multiple processes are running in the parent function.
+ 
+ 
+in above eg, each process exits very soon after it starts, so there’s little overlap in time. Let’s do something slightly more sophisticated to better illustrate multiple forked processes running in parallel. 
+
+"""
+fork basics: start 5 copies of this program running in parallel with the original; each copy counts up to 5 on the same 
+stdout stream--forks copy process memory, including file descriptors; fork doesn't currently work on Windows without Cygwin: 
+use os.spawnv or multiprocessing on Windows instead; spawnv is roughly like a fork+exec combination;
+"""
+import os, time
+
+def counter(count):
+	for i in range(count):
+		time.sleep(1)
+		print('[%s] => %s' % (os.getpid(), i))
+	
+ for i in range(5): 
+  pid = os.fork()
+		if pid != 0:
+			print('Process %d spawned' % pid)
+		else: 
+   counter(5)
+			os._exit(0) 
+   print('Main process exiting.')
+	# run in new process # simulate real work
+	# in parent: continue
+	# else in child/new process # run function and exit
+	# parent need not wait
+ 
+When run, this script starts 5 processes immediately and exits. All 5 forked processes check in with their first count display one second later and every second thereafter. Notice that child processes continue to run, even if the parent process that created them terminates:
+ The output of all of these processes shows up on the same screen, because all of them share the standard output stream (and a system prompt may show up along the way, too). 
+ 
+ Technically, a forked process gets a copy of the original process’s global memory, including open file descriptors. Because of that, global objects like files start out with the same values in a child process, so all the processes here are tied to the same single stream. But it’s important to remember that global memory is copied, not shared; if a child process changes a global object, it changes only its own copy. (As we’ll see, this works differently in threads,
+ 
